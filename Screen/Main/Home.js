a3cfda3432545d26df34cc8e1ba5b8051ea1f92b
sakibsarekr <sakib15-3824@diu.edu.bd>
2022-06-20 03:51:02 +0600

    blog page data conneting with firebase

--- Screen/Main/Home.js
+++ Screen/Main/Home.js
@@ -1,13 +1,84 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import {View,Text,FlatList,StyleSheet,Modal} from 'react-native'
-import { Icon } from 'react-native-vector-icons/Icon'
 import auth from '@react-native-firebase/auth'
 import firestore from '@react-native-firebase/firestore'
 import { globalStyles } from '../../urils/global'
+import ModalView from '../../components/ModalView'
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import BlogCard from '../../components/BlogCard'
+
 export default function Home() {
+
+  const [blogs,setBlogs]=useState([])
+  const [modalOpen,setModalOPen]=useState(false)
+  const [selectedCardId,setSelectedCardId]=useState([])
+
+  function getBlogData() {
+    firestore().collection('usersBlog')
+    .doc(auth().currentUser.uid)
+    .collection('blogs')
+    .onSnapshot((quearySnapshot) => {
+       const data = []
+       quearySnapshot.forEach((documentSnapshot) => {
+          data.push({
+             ...documentSnapshot.data(),
+             id: documentSnapshot.id
+          })
+       })
+       setBlogs(data)
+    })
+ }
+
+ useEffect(() => {
+  getBlogData()
+}, [])
+
+function renderItem({ item }) {
+  return(
+     <BlogCard 
+        blogData={item}
+     />
+  )
+}
+
   return (
-    <View>
-        
+    <View style={globalStyles.primaryContainer}>
+        <Modal
+        visible={modalOpen}
+        animationType='fade'
+        transparent={true}
+        >
+          <ModalView
+          />
+
+        </Modal>
+        <View style={styles.header}>
+          <Text style={globalStyles.headingText}>My Blog</Text>
+        </View>
+        <Ionicons
+        style={styles.addIcon}
+         name="add-circle-sharp" 
+         size={54}
+         color='red'
+         />
+          <View style={{ alignItems: 'center' }}>
+            <FlatList 
+               data={blogs}
+               keyExtractor={(item) => item.id}
+               renderItem={renderItem}
+            />
+         </View>
     </View>
   )
 }
+const styles=StyleSheet.create({
+  header:{
+    marginHorizontal:10,
+    marginVertical:10
+  },
+  addIcon:{
+    position:'absolute',
+    bottom:20,
+    left:'45%'
+  }
+})
